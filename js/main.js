850bcdd4aef1f9067cb10179931e7c5c3ac620bc
Pascal Laliberté <pascal.laliberte@gmail.com>
2014-09-06 22:00:09 -0400

    Permis l'utilisation du bouton Back et réparé Github pages - Employé pushState, popstate pour déterminé l'état de la page - Opté pour employer des pages pour les lectures additionnelles au lieu d'une collection (répare Github Pages)

--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
 $( document ).ready(function() {
-  $.get( "/additionnelles/2014-09-07-dimanche.html", function( data ) {
+
+  $(window).on("popstate", function(e) {
+    enable_additional_readings();
+    $( ".post-additional-readings").eq(0).remove();
+  });
+
+});
+
+enable_additional_readings = function () {
+  readings_url = "/additionnelles/2014-09-07-dimanche.html";
+  $.get( readings_url, function( data ) {
+
     $('.home .post-header').eq(0).each(function(){
-      $(this).append('<div id="post-additional-readings-toggle"><a id="post-additional-readings-toggle-link" href="/additionnelles/2014-09-07-dimanche.html">Afficher les autres textes</a></div>');
+      $(this).append('<div id="post-additional-readings-toggle"><a id="post-additional-readings-toggle-link" href="' + readings_url + '">Afficher les autres textes</a></div>');
 
       $('#post-additional-readings-toggle-link').click(function(event) {
         event.preventDefault();
         $( ".post-content" ).eq(0).prepend('<div class="post-additional-readings"></div>')
-        $( ".post-additional-readings").eq(0).html( data ).slideDown();
         $( '#post-additional-readings-toggle').remove();
+        $( ".post-additional-readings").eq(0).html( data ).slideDown();
+        history.pushState({}, null, '#additionnelles');
       });
 
     });
   });
-});
+}
