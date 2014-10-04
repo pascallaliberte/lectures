bbfa52f6a26616d5c7bcdde978186aff7d083071
Pascal Laliberté <pascal.laliberte@gmail.com>
2014-10-04 15:26:38 -0400

    Adapté à tirer le contenu de l'Évangile via un appel ajax

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
 $( document ).ready(function() {
 
   $(window).on("popstate", function(e) {
+    enable_main_readings();
     enable_additional_readings();
     $( ".post-readings-additional").eq(0).remove();
   });
 
 });
 
+enable_main_readings = function () {
+  readings_url = "/{date}/";
+  readings_date = $('.home').eq(0).data('post-date');
+  readings_url = readings_url.replace(/\{date\}/, readings_date);
+
+  $.get( readings_url, function( data ) {
+
+    $('.home').eq(0).each(function(){
+      $(this).html( data );
+    });
+  });
+}
+
 enable_additional_readings = function () {
   readings_url = "/additionnelles/{date}.html";
-  readings_date = $('.post-meta .post-date').data('post-date');
+  readings_date = $('.home').eq(0).data('post-date');
   readings_url = readings_url.replace(/\{date\}/, readings_date);
 
   $.get( readings_url, function( data ) {
