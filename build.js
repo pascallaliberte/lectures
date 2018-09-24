7a4cd7bed744aaf4d6bb266602c70da0cd5e8fc9
Pascal Laliberté <pascal@pascallaliberte.me>
2018-09-23 20:22:31 -0400

    add webpack, build-js script

--- build.js
+++ build.js
@@ -11,7 +11,6 @@ rimraf.sync(dist)
 
 // copy the public directories
 fs.copySync('public/', dist)
-fs.copySync('js/', dist + 'js/')
 fs.copySync('fonts/', dist + 'fonts/')
 fs.copySync('bower_components/', dist + 'bower_components/')
 
